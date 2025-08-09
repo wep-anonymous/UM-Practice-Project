4cd382970696fb66de6b30960b40a5a5e6b91f7e
wep-anonymous <108601395+wep-anonymous@users.noreply.github.com>
2025-08-09 19:15:50 +0800

    feat: create Vue application entry point

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>UM Practice Project</h1>
+    <p>Vue 2 Application</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 50px;
+}
+</style>
