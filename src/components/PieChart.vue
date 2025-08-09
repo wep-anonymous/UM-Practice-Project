33de194874a446bc24f8aedd35756edced94b3c7
wep-anonymous <108601395+wep-anonymous@users.noreply.github.com>
2025-08-10 00:13:58 +0800

    feat: create pie chart component with rounded segments

---
+++ src/components/PieChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="chart" style="width: 100%; height: 400px;"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+  name: 'PieChart',
+  mounted() {
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      const chart = echarts.init(this.$refs.chart);
+      const option = {
+        series: [{
+          type: 'pie',
+          radius: ['40%', '70%'],
+          itemStyle: {
+            borderRadius: 8,
+            borderColor: '#fff',
+            borderWidth: 3
+          },
+          data: [
+            { value: 1048, name: 'Chrome' },
+            { value: 735, name: 'Firefox' },
+            { value: 580, name: 'Safari' },
+            { value: 484, name: 'Edge' },
+            { value: 300, name: 'Others' }
+          ]
+        }]
+      };
+      chart.setOption(option);
+    }
+  }
+}
+</script>
